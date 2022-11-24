1cc0e85ebff9cf3e0e4129a9756471981ccbcc41
vo <vo@homedeb.local>
2022-11-25 00:04:50 +0300

    fix ip2num add processing

--- process.lua
+++ process.lua
@@ -1,10 +1,16 @@
 #!/usr/bin/luajit
 
+string = require('string')
+
 -------------------------------------------------
 function ip2num(s)
-  local a
+  local a = {}
+  local i = 1
   local n
-  a = string.split(tostring(s),'.')                                                                                                                                                                     
+  for d in string.gmatch(tostring(s),'(%d+)') do 
+    a[i]=tonumber(d)
+    i=i+1
+  end
   n = a[1]*16777216+a[2]*65536+a[3]*256+a[4]
   return n
 end
@@ -22,24 +28,23 @@ end
 function processIPs ()
   local file = "./out_sa.csv"
   for line in io.lines(file) do 
---    print(line)
-    for i,c in string.gmatch(line,"(%w+),(%w+)") do
-      print("i="..i.." c="..c)
+    for ip,c in string.gmatch(line,"(%g+) (%g+)") do
+      n=ip2num(ip)
+      as=asn[n]
+      if as == nil then
+        as=0
+      end
+      print("ip="..ip.." as="..as.." c="..c)
     end
   end
   
 end
 ---------------------- BEGIN --------------------
 asn={}
---loadASn()
+loadASn()
 processIPs()
 
-
 --print("main: asn[3758095360]="..asn[3758095360])
-
---foreach $as (@ips) {
---  print "[".$as."] [".$ips[$ip]."]\n";
---}
-
---$ip="217.21.34.0/24";
---print $ip." ".$ips[$ip]."\n";
+c='217.21.34.0'
+print(ip2num(c))
+print(asn[ip2num(c)])
